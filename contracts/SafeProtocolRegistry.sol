b4196a7eaca52a837e7412487f960bb8e6070cbb
Akshay <akshay.ap95@gmail.com>
2023-06-16 15:19:47 +0200

    [#3] Create SafeProtocolRegistry interface and contract

---
+++ contracts/SafeProtocolRegistry.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+import "./interfaces/ISafeProtocolRegistry.sol";
+contract SafeProtocolRegistry is ISafeProtocolRegistry {
+    
+    function check(address component) external view returns (uint256 listedAt, uint256 flaggedAt){
+        
+    }
+}
