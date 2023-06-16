d79567d6e18cd6a99eee087698a05d31925d6349
Akshay <akshay.ap95@gmail.com>
2023-06-16 14:27:33 +0200

    [#2] Remove ISafeProtocolRootAccess interface

--- contracts/interfaces/components/ISafeProtocolGuard.sol
+++ contracts/interfaces/components/ISafeProtocolGuard.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 import "../../external/interfaces/ISafe.sol";
-import "../ISafeRootAccess.sol";
 import "../../libraries/DataTypes.sol";
 
 interface ISafeProtocolGuard {
