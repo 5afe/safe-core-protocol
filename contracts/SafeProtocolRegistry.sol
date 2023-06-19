74ee9c869581f77982b1851c354e4ff70318ba93
Akshay <akshay.ap95@gmail.com>
2023-06-19 20:26:42 +0200

    [#3] Add natspec docstring

--- contracts/SafeProtocolRegistry.sol
+++ contracts/SafeProtocolRegistry.sol
@@ -4,6 +4,11 @@ import "./interfaces/ISafeProtocolRegistry.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 contract SafeProtocolRegistry is ISafeProtocolRegistry, Ownable2Step {
+
+    /**
+     * @dev TODO: Determint whether there exists a more gas efficient way to store component info based on the way component information is accessed.
+     *      For simplicity, currently using a struct. 
+     */
     mapping(address => ComponentInfo) listedComponents;
 
     struct ComponentInfo {
@@ -18,12 +23,24 @@ contract SafeProtocolRegistry is ISafeProtocolRegistry, Ownable2Step {
         _transferOwnership(initialOwner);
     }
 
+    /**
+     * @notice This function returns information about a component
+     * @param component Address of the component to be checked
+     * @return listedAt Timestamp of listing the component. This value will be 0 if not listed.
+     * @return flaggedAt Timestamp of falgging the component. This value will be 0 if not flagged.
+     */
     function check(address component) external view returns (uint64 listedAt, uint64 flaggedAt) {
         ComponentInfo memory componentInfo = listedComponents[component];
         listedAt = componentInfo.listedAt;
         flaggedAt = componentInfo.flaggedAt;
     }
 
+    /**
+     * @notice Allows only owner to add a component. A component can be any address including zero address for now.
+     *         This function does not permit adding a component twice.
+     *         TODO: Add logic to validate if component implements correct interface.
+     * @param component Address of the component
+     */
     function addComponent(address component) external onlyOwner {
         ComponentInfo memory componentInfo = listedComponents[component];
 
@@ -33,13 +50,19 @@ contract SafeProtocolRegistry is ISafeProtocolRegistry, Ownable2Step {
         listedComponents[component] = ComponentInfo(uint64(block.timestamp), 0);
     }
 
+    /**
+     * @notice Allows only owner to flad a component. Only previously added component can be flagged.
+     *         This function does not permit flagging a component twice.
+     *         A component can be any address including zero address for now.
+     * @param component Address of the component
+     */
     function flagComponent(address component) external onlyOwner {
         ComponentInfo memory componentInfo = listedComponents[component];
 
         if (componentInfo.listedAt == 0 || componentInfo.flaggedAt != 0) {
             revert CannotFlagComponent(component);
         }
-        // TODO: Find a better way to do this
+        // TODO: Determint whether there exists a more gas efficient way to update component info.
         listedComponents[component] = ComponentInfo(componentInfo.listedAt, uint64(block.timestamp));
     }
 }
