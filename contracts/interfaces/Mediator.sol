bf7c6288d0ed14f90652e9dd006d843e1fd85b7f
Akshay <akshay.ap95@gmail.com>
2023-06-20 11:45:43 +0200

    [#4] Create mediator interface

---
+++ contracts/interfaces/Mediator.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.18;
+import "./Accounts.sol";
+import "../DataTypes.sol";
+
+/**
+ * @title ISafeProtocolMediator interface that a Mediator should implement
+ * @notice A mediator checks the status of the component through the registry and allows only
+ *         listed and non-flagged components to execute transactions. A Safe account should
+ *         add mediator as a module.
+ */
+interface ISafeProtocolMediator {
+    /**
+     * @notice TODO
+     * @param safe Instance of a Safe account
+     * @param tx SafeTransaction instance containing payload information about the transaction
+     * @return success boolean indicating status of execution.
+     * @return data Arbitrary length bytes data returned upon the successful execution.
+     *         Empty if the call failed.
+     */
+    function executeTransaction(ISafe safe, SafeTransaction calldata tx) external view returns (bool success, bytes memory data);
+
+    /**
+     * @notice TODO
+     * @param safe Instance of a Safe account
+     * @param rootAccess SafeTransaction instance containing payload information about the transaction
+     * @return success boolean indicating status of execution.
+     * @return data Arbitrary length bytes data returned upon the successful execution.
+     *         Empty if the call failed.
+     */
+    function executeRootAccess(ISafe safe, SafeRootAccess calldata rootAccess) external view returns (bool success, bytes memory data);
+}
