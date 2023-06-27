d8b4c4ced06dd0fd2919a887edef7c94230a0eb5
Akshay <akshay.ap95@gmail.com>
2023-06-27 13:24:39 +0200

    [#4] Remove setModule from ISafe interface

--- contracts/interfaces/Accounts.sol
+++ contracts/interfaces/Accounts.sol
@@ -5,8 +5,6 @@ pragma solidity ^0.8.18;
  * @title ISafe Declares the functions that are called on a Safe by Safe protocol.
  */
 interface ISafe {
-    function setModule(address _module) external;
-
     function execTransactionFromModule(
         address payable to,
         uint256 value,
