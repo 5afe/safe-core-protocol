41de66344974093f17c4d8702d93e2565de6e8b1
Akshay <akshay.ap95@gmail.com>
2023-07-06 15:53:59 +0200

    Add test case: Revert when creating registrymanager with invalid registry not implementing expected interfaceId

--- test/base/RegistryManager.spec.ts
+++ test/base/RegistryManager.spec.ts
@@ -32,10 +32,16 @@ describe("RegistryManager", async () => {
     it("Should revert with AccountDoesNotImplementValidInterfaceId when registry address does not implement valid interfaceId", async () => {
         const { registryManager } = await loadFixture(deployContractsFixture);
         const registry = await getMockRegistryWithInvalidInterfaceSupport();
-        await expect(registryManager.connect(owner).setRegistry(registry)).to.be.revertedWithCustomError(
+        await expect(registryManager.connect(owner).setRegistry(await registry.getAddress())).to.be.revertedWithCustomError(
             registryManager,
             "AccountDoesNotImplementValidInterfaceId",
         );
+    });
+
+    it("Should revert with AccountDoesNotImplementValidInterfaceId when creating registry manager with registry not supporting valid interfaceId", async () => {
+        const registry = await getMockRegistryWithInvalidInterfaceSupport();
+        await expect(hre.ethers.deployContract("RegistryManager", [await registry.getAddress(), owner.address], { signer: deployer })).to.be
+            .reverted;
     });
 
     it("Should emit RegistryChanged change event when registry is updated", async () => {
