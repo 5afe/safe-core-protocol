64721a9920af6a9b4ad828b28cb97a72554a6ecc
Akshay <akshay.ap95@gmail.com>
2023-07-14 17:14:04 +0200

    Fix lint issue

--- contracts/test/TestSafeProtocolManager.sol
+++ contracts/test/TestSafeProtocolManager.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.18;
-import { SafeProtocolManager } from "../SafeProtocolManager.sol";
+import {SafeProtocolManager} from "../SafeProtocolManager.sol";
 
 /**
  * @title This is a test version of SafeProtocolManager and should use TestSafeProtocolRegistryUnrestricted contract as resgistry.
  */
 contract TestSafeProtocolManager is SafeProtocolManager {
-    constructor(address initialOwner, address registry) SafeProtocolManager(initialOwner, registry) {} 
+    constructor(address initialOwner, address registry) SafeProtocolManager(initialOwner, registry) {}
 }
