cc7513c134d184dd7b103ac87f76a70c32b5df03
Akshay <akshay.ap95@gmail.com>
2023-08-15 13:56:39 +0200

    [#47] Update tests for Function Handler

--- test/FunctionHandlerManager.spec.ts
+++ test/FunctionHandlerManager.spec.ts
@@ -3,15 +3,14 @@ import hre, { deployments, ethers } from "hardhat";
 import { getMockFunctionHandler } from "./utils/mockFunctionHandlerBuilder";
 import { IntegrationType } from "./utils/constants";
 import { expect } from "chai";
-import { TestStaticFunctionHandler } from "../typechain-types";
-import { getInstance, getMockTestExecutorInstance } from "./utils/contracts";
+import { getMockTestExecutorInstance } from "./utils/contracts";
 import { MaxUint256 } from "ethers";
 
 describe("Test Function Handler", async () => {
-    let deployer: SignerWithAddress, owner: SignerWithAddress;
+    let deployer: SignerWithAddress, owner: SignerWithAddress, user1: SignerWithAddress;
 
     before(async () => {
-        [deployer, owner] = await hre.ethers.getSigners();
+        [deployer, owner, user1] = await hre.ethers.getSigners();
     });
 
     const setupTests = deployments.createFixture(async ({ deployments }) => {
@@ -28,15 +27,14 @@ describe("Test Function Handler", async () => {
 
         await safeProtocolRegistry.addIntegration(mockFunctionHandler.target, IntegrationType.FunctionHandler);
 
-        const testFunctionHandler = await ethers.deployContract("TestFunctionHandler", { signer: deployer });
-        await safeProtocolRegistry.addIntegration(testFunctionHandler.target, IntegrationType.FunctionHandler);
+        const testFunctionHandler = await ethers.deployContract("MockContract", { signer: deployer });
+        await testFunctionHandler.givenMethodReturnBool("0x01ffc9a7", true);
 
-        const testStaticFunctionHandler = await ethers.deployContract("TestStaticFunctionHandler", { signer: deployer });
-        await safeProtocolRegistry.addIntegration(testStaticFunctionHandler.target, IntegrationType.FunctionHandler);
+        await safeProtocolRegistry.addIntegration(testFunctionHandler.target, IntegrationType.FunctionHandler);
 
         const safe = await getMockTestExecutorInstance();
 
-        return { safe, functionHandlerManager, mockFunctionHandler, safeProtocolRegistry, testFunctionHandler, testStaticFunctionHandler };
+        return { safe, functionHandlerManager, mockFunctionHandler, safeProtocolRegistry, testFunctionHandler };
     });
 
     it("Should emit FunctionHandlerChanged event when Function Handler is set", async () => {
@@ -71,58 +69,39 @@ describe("Test Function Handler", async () => {
     });
 
     it("Should revert with FunctionHandlerNotSet when function handler is not enabled", async () => {
-        const { safe, functionHandlerManager } = await setupTests();
+        const { functionHandlerManager } = await setupTests();
 
         const data = "0x00000000";
 
         await expect(
-            deployer.sendTransaction({
-                to: safe.target,
+            user1.sendTransaction({
+                to: functionHandlerManager.target,
                 value: 0,
                 data: data,
             }),
         )
             .to.be.revertedWithCustomError(functionHandlerManager, "FunctionHandlerNotSet")
-            .withArgs(safe.target, data);
+            .withArgs(user1.address, data);
     });
 
     it("Should call handle function of function handler", async () => {
-        const { safe, functionHandlerManager, testFunctionHandler } = await setupTests();
+        const { functionHandlerManager, testFunctionHandler } = await setupTests();
 
         // 0xf8a8fd6d -> function test() external {}
         const data = "0xf8a8fd6d";
 
-        const dataSetFunctionHandler = functionHandlerManager.interface.encodeFunctionData("setFunctionHandler", [
-            data,
-            testFunctionHandler.target,
-        ]);
-        await safe.executeCallViaMock(functionHandlerManager, 0n, dataSetFunctionHandler, MaxUint256);
+        await functionHandlerManager.connect(user1).setFunctionHandler(data, testFunctionHandler.target);
+        await testFunctionHandler.givenMethodReturnBool("0xe962001f", true);
+
         await (
-            await deployer.sendTransaction({
-                to: safe.target,
+            await user1.sendTransaction({
+                to: functionHandlerManager.target,
                 value: 0,
-                data: data,
+                data: data + user1.address.slice(2), // Handler expects additional 20 bytes data at the end that indicates original sender of transaction.
             })
         ).wait();
 
-        expect(await testFunctionHandler.inc()).to.equal(1);
-    });
-
-    it("Should call to handle function of function handler (static)", async () => {
-        const { safe, functionHandlerManager, testStaticFunctionHandler } = await setupTests();
-        // 0xf8a8fd6d -> function test() external {}
-        const data = "0xf8a8fd6d";
-
-        const dataSetFunctionHandler = functionHandlerManager.interface.encodeFunctionData("setFunctionHandler", [
-            data,
-            testStaticFunctionHandler.target,
-        ]);
-
-        await safe.executeCallViaMock(functionHandlerManager, 0n, dataSetFunctionHandler, MaxUint256);
-        // Attach Safe to a contract with test function in the ABI
-        const testSafe = await getInstance<TestStaticFunctionHandler>("TestStaticFunctionHandler", safe.target);
-
-        // 0x3078626164646164 -> abi.encode("0xbaddad")
-        expect(await testSafe.test.staticCall()).to.be.eq("0x3078626164646164");
+        expect(await testFunctionHandler.invocationCountForMethod("0x25d6803f")).to.equal(1n);
+        expect(await testFunctionHandler.invocationCount()).to.equal(1);
     });
 });
