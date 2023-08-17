11ce14fa0732575bb01f0b643c3b1a814682015b
Akshay <akshay.ap95@gmail.com>
2023-08-17 11:42:50 +0200

    [#47] Add ERC165 check while adding function handler

--- contracts/base/FunctionHandlerManager.sol
+++ contracts/base/FunctionHandlerManager.sol
@@ -40,7 +40,11 @@ abstract contract FunctionHandlerManager is RegistryManager {
      * @param functionHandler Address of the contract to be set as a function handler
      */
     function setFunctionHandler(bytes4 selector, address functionHandler) external {
-        if (functionHandler != address(0)) checkPermittedIntegration(functionHandler);
+        if (functionHandler != address(0)) {
+            checkPermittedIntegration(functionHandler);
+            if (!ISafeProtocolFunctionHandler(functionHandler).supportsInterface(type(ISafeProtocolFunctionHandler).interfaceId))
+                revert AccountDoesNotImplementValidInterfaceId(functionHandler);
+        }
 
         // No need to check if functionHandler implements expected interfaceId as check will be done when adding to registry.
         functionHandlers[msg.sender][selector] = functionHandler;
