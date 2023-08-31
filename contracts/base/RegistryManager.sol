17f9c547e2eabb5cd9f3d8fe541b0cc9110e888f
Akshay <akshay.ap95@gmail.com>
2023-08-31 16:02:36 +0200

    [#75] Add onlyAccount function modifier to setRegistry(address)

--- contracts/base/RegistryManager.sol
+++ contracts/base/RegistryManager.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.18;
 import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {ISafeProtocolRegistry} from "../interfaces/Registry.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {OnlyAccountCallable} from "./OnlyAccountCallable.sol";
 
-contract RegistryManager is Ownable2Step {
+contract RegistryManager is Ownable2Step, OnlyAccountCallable {
     address public registry;
 
     event RegistryChanged(address indexed oldRegistry, address indexed newRegistry);
@@ -19,14 +20,12 @@ contract RegistryManager is Ownable2Step {
 
     /*
      * @notice Constructor that sets registry address and owner address.
-     * @dev Do not set owner as a Safe address with Manager enabled as fallback handler.
-     *      If owner is a Safe with Manager enabled as fallback handler, then a malicious
-     *      address can call Safe with calldata that updates registry which gets forwarded to Manager.
-     * @param intitalOwner Address of the owner for this contract.
+     * @param initialOwner Address of the owner for this contract.
+     *        Owner is expected to be an account that appends 20 bytes of caller (i.e. self) address to the calldata when calling setRegistry(address) function.
      * @param _registry address of the account implementing ISafeProtocolRegistry interface.
      */
-    constructor(address _registry, address intitalOwner) {
-        _transferOwnership(intitalOwner);
+    constructor(address _registry, address initialOwner) {
+        _transferOwnership(initialOwner);
         if (!IERC165(_registry).supportsInterface(type(ISafeProtocolRegistry).interfaceId)) {
             revert ContractDoesNotImplementValidInterfaceId(_registry);
         }
@@ -50,7 +49,7 @@ contract RegistryManager is Ownable2Step {
      * @notice Allows only owner to update the address of a registry. Emits event RegistryChanged(egistry, newRegistry)
      * @param newRegistry Address of new registry
      */
-    function setRegistry(address newRegistry) external onlyOwner {
+    function setRegistry(address newRegistry) external onlyOwner onlyAccount {
         if (!IERC165(newRegistry).supportsInterface(type(ISafeProtocolRegistry).interfaceId)) {
             revert ContractDoesNotImplementValidInterfaceId(newRegistry);
         }
