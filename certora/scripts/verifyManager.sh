40f1b8cf76551a5ea0a229696408791c5ef02501
Akshay <akshay.ap95@gmail.com>
2023-09-18 10:40:22 +0200

    [#95] Add param --solc in verifyManager.sh

--- certora/scripts/verifyManager.sh
+++ certora/scripts/verifyManager.sh
@@ -8,6 +8,7 @@ fi
 
 certoraRun contracts/SafeProtocolManager.sol \
     --verify SafeProtocolManager:certora/specs/Manager.spec \
+    --solc solc8.18 \
     --optimistic_loop \
     --loop_iter 3 \
     --optimistic_hashing \
