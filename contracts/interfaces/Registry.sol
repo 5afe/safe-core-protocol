2447eefc7a4a4206933389ccd43b58867f6357eb
Akshay <akshay.ap95@gmail.com>
2023-09-20 15:22:01 +0200

    [#103] Update natspec doc

--- contracts/interfaces/Registry.sol
+++ contracts/interfaces/Registry.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.8.18;
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 interface ISafeProtocolRegistry is IERC165 {
-    /// @param module Address of the module that should be checked
-    /// @return listedAt MUST return the block number when the module was listed in the registry (or 0 if not listed)
-    /// @return flaggedAt MUST return the block number when the module was listed in the flagged as faulty (or 0 if not flagged)
-    /// @param moduleTypes uint8 indicating the type of module.
+    /** @param module Address of the module that should be checked
+     * @return listedAt MUST return the block number when the module was listed in the registry (or 0 if not listed)
+     * @return flaggedAt MUST return the block number when the module was listed in the flagged as faulty (or 0 if not flagged)
+     * @return moduleTypes uint8 indicating the types of module that the contract can be used as in the protocol.
+     *                     The value is a bitwise OR of the module types. For example, if the module can be used as a plugin and
+     *                     a function handler, the value will be 2^0 (Plugin) + 2^1 (Function Handler) = 3.
+     */
     function check(address module) external view returns (uint64 listedAt, uint64 flaggedAt, uint8 moduleTypes);
 }
