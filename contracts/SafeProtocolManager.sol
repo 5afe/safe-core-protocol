9ec4cc6718cb308593d197ef4c22286407ecd428
Nicholas Rodrigues Lordello <nick@safe.global>
2023-11-09 16:26:03 +0100

    Check that it is a pluggin as well

--- contracts/SafeProtocolManager.sol
+++ contracts/SafeProtocolManager.sol
@@ -465,7 +465,11 @@ contract SafeProtocolManager is ISafeProtocolManager, RegistryManager, HooksMana
         }
     }
 
-    function transferPrefund(address account, address payable entrypoint, uint256 missingAccountFunds) external override {
+    function transferPrefund(
+        address account,
+        address payable entrypoint,
+        uint256 missingAccountFunds
+    ) external override onlyEnabledPlugin(account) {
         require(functionHandlers[hex"3a871cdd"][account] == msg.sender);
         IAccount(account).execTransactionFromModule(entrypoint, missingAccountFunds, "", 0);
     }
